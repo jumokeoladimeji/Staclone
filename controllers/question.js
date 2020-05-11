0a96c12e3bb8c054201f7c30185a1afbecbd26f8
jumoke <i.jumokeoladimeji@gmail.com>
2020-05-11 05:37:48 +0100

    fix upvote downvote and add answer bugs

--- controllers/question.js
+++ controllers/question.js
@@ -39,7 +39,6 @@ const removeDownvoteAndIncrementCount = (user, question, res, incrBy) => {
                 data: `error upvoting question: ${err}` 
             })
         } else {
-            console.log('question', question)
              // delete voter document from upvote
              Downvote.updateOne({
                 questionId: question._id,
@@ -49,8 +48,6 @@ const removeDownvoteAndIncrementCount = (user, question, res, incrBy) => {
                         voters: mongoose.Types.ObjectId(user._id)
                     }
                 }, (err, retvalue) => {
-                    console.log('retvalue:::', retvalue);
-
                     question.voteCount += incrBy;
                     return res.status(200).json({ 
                         message: 'Question successfully negated downvote',
@@ -69,7 +66,7 @@ module.exports = {
      * @returns {json} question - fetched question
      */
     get(req, res) {
-        Question.findById(req.params.questionId).populate('user').populate({path: 'answers'}).populate({path: 'upvotes'}).populate({path: 'upvotes'}).exec((err, question) => {
+        Question.findById(req.params.questionId).populate('user').populate('answers').exec((err, question) => {
             if (err) {
                 return res.status(500).json({ data: 'error saving question:' + err })
             }
@@ -83,7 +80,7 @@ module.exports = {
      * @returns {json} questions - questions fetched
      */
     getAll: function(req, res) {
-        Question.find({}).populate('user').populate({path: 'answers'}).populate({path: 'upvotes'}).populate({path: 'upvotes'}).exec((err, questions) => {
+        Question.find({}).populate('user').populate('answers').exec((err, questions) => {
             if (err) {
                 return res.status(500).json({ data: 'error getting questions:' + err })
             } else {
@@ -113,15 +110,6 @@ module.exports = {
                 return res.status(500).json({ data: 'error saving question:' + err })
             }
               else {
-                        
-                // const newUpvote = {};
-                // newQuestion.voters = [];
-                // newUpvote.voters.push(req.user._id);
-                // newUpvote.questionId = req.question._id;
-                // console.log('newUpvote', newUpvote)
-                // const updatedUpvote = new Upvote(newUpvote)
-                // console.log('updatedUpvote', updatedUpvote)
-
                 return res.status(200).json({
                     message: "Question successfully created",
                     data: createdQuestion
@@ -168,20 +156,25 @@ module.exports = {
     * @returns {json} message, response or error
     */
     async addAnswer (req, res) {
-        const question = req.question;
+        const question = req.question;     
+        if (!req.body) {
+            return res.status(422).send({
+                message: 'question needs an answer'
+            }); 
+        }
         let newAnswer = {
-            answer: req.body,
-            user: req.user
+            answer: req.body.answer,
+            user: req.user._id
         };
         let hasAnsweredBefore = false;
 
-        if (req.user.id === question.user._id.toString()) {
+        if (req.user._id.toString() === question.user._id.toString()) {
             return res.status(400).send({
                 message: 'You cannot answer your own question'
             });
         } else {
             for (let i = 0; i < question.answers.length; i++) {
-                if (req.user.id === question.answers[i].user) {
+                if (req.user._id.toString() === question.answers[i].user._id.toString()) {
                     hasAnsweredBefore = true;
                     return  res.status(500).json({ data: 'you have already answerred this question' })
                 }
@@ -191,18 +184,17 @@ module.exports = {
 
                 answer.save((err) => {
                     if (err) {
-                        return res.status(500).json({ data: 'error saving answer' })
+                        return res.status(500).json({ data: 'error saving answer: ' + err })
                     }
                   
-                    question.answers.push(answer);
+                    question.answers.push(answer._id);
                     
                     question.save((err) => {
                         if (err) {
                             return res.status(500).json({ data: 'error saving answer to question' })
                         } else {
-                            const questionLink = `/api/v1/users/${req.params.id}/questions/${req.params.questionId}`
-                            sendEmail.notify(req, res, questionLink, req.user);
-                    
+                            const questionLink = `http://${req.headers.host}/api/v1/questions/${req.params.questionId}`;
+                            sendEmail.notify(res, req.user, questionLink, question);
                         }
                     });
                 });
@@ -219,9 +211,10 @@ module.exports = {
         const votedQuestion = req.question;
         let newUpvote = {};
         let hasupvoted = false;
+        let hasdownvoted = false;
         const questionId = req.params.questionId
 
-        Upvote.findOne({ questionId: questionId }).populate({path: 'voters'}).exec((error, upvote) => {
+        Upvote.findOne({ questionId: questionId }).populate('voters').exec((error, upvote) => {
             if (error) {
                 return res.status(500).json({ data: `error upvoting question: ${error}` })
             }
@@ -235,11 +228,11 @@ module.exports = {
                     }
                 }
             }  else {
-                const downVoteToCreate = {
+                const upVoteToCreate = {
                     voters: [],
                     questionId: req.question._id,
                 };
-                newDownvote = new Downvote(downVoteToCreate)
+                newUpvote = new Upvote(upVoteToCreate)
             }  
     
             if (!hasupvoted) {
@@ -250,18 +243,17 @@ module.exports = {
                     if (err) {
                         return res.status(500).json({ data: 'error saving new upvote' + err })
                     } 
-                });    
-            // check if has downvoted
-                Downvote.findOne({ questionId: questionId }).populate({path: 'voters'}).exec((err, downvote) => {
-                    if (err) {
-                        return res.status(500).json({ data: `error upvoting question: ${err}` }) 
-                    }
+                    // check if has downvoted
+                    Downvote.findOne({ questionId: questionId }).populate('voters').exec((err, downvote) => {
+                        if (err) {
+                            return res.status(500).json({ data: `error while returning question downvote: ${err}` }) 
+                        }
 
-                    if(downvote) {
-                        for (let i = 0; i < downvote.voters.length; i++) {
-                            if (req.user._id.toString() === downvote.voters[i]._id.toString()) {
+                        if(downvote) {
+                            for (let i = 0; i < downvote.voters.length; i++) {
 
-                                Question.updateOne({_id: questionId},{ $inc: { voteCount: 1 }}).then(() => {
+                                if (req.user._id.toString() === downvote.voters[i]._id.toString()) {
+                                    hasdownvoted = true;
 
                                     Downvote.updateOne({
                                         questionId: questionId,
@@ -270,24 +262,35 @@ module.exports = {
                                             voters: mongoose.Types.ObjectId(req.user._id)
                                         }
                                     }, (err) => {
-                                        return res.status(200).json({ 
-                                            message: 'Question successfully upvoted',
-                                            data: votedQuestion })
-                                    })
-                                }).catch(err => {
-                                    return res.status(500).json({ data: `error upvoting question: ${err}` }) 
-                                });
-                                break;
+                                        Question.updateOne({_id: questionId},{ $inc: { voteCount: 2 }}).then(() => {
+
+                                            return res.status(200).json({ 
+                                                message: 'Question successfully upvoted',
+                                                data: votedQuestion })
+                                        }).catch(err => {
+                                            return res.status(500).json({ data: `error upvoting question: ${err}` }) 
+                                        });
+                                    });
+
+                                    break;
+                                }
                             }
+                        }  
+
+                        if(!hasdownvoted) {
+
+                            votedQuestion.voteCount++;
+                            votedQuestion.save((err) => {
+                                if (err) {
+                                    return res.status(500).json({ data: `error upvoting question: ${err}` })
+                                } 
+                                return res.status(200).json({ 
+                                    message: 'Question successfully upvoted',
+                                    data: votedQuestion 
+                                })
+                            });
                         }
-                    } else {
-                        votedQuestion.voteCount++;
-                        votedQuestion.save((err) => {
-                            if (err) {
-                                return res.status(500).json({ data: `error upvoting question: ${err}` })
-                            } 
-                        });
-                    }
+                    });
                 });
             }
         })
@@ -301,11 +304,10 @@ module.exports = {
     downvote (req, res) {
         const votedQuestion = req.question;
         let newDownvote = {};
-        // let hasupvoted = false;
+        let hasupvoted = false;
         let hasdownvoted = false;
 
-        Downvote.findOne({ questionId: req.params.questionId }).populate({path: 'voters'}).exec((error, downvote) => {
-            console.log('upvote gotten', downvote)
+        Downvote.findOne({ questionId: req.params.questionId }).populate('voters').exec((error, downvote) => {
             if (error) {
                 return res.status(500).json({ data: `error downvoting question: ${error}` })
             }
@@ -319,90 +321,80 @@ module.exports = {
                         break;
                     }
                 }
-            }  
+            } else {
+                const downVoteToCreate = {
+                    voters: [],
+                    questionId: req.question._id,
+                };
+                newDownvote = new Downvote(downVoteToCreate)
+
+            }
             if (!hasdownvoted) {
-                if(!downvote) {
-                    const downVoteToCreate = {
-                        voters: [],
-                        questionId: req.question._id,
-                    };
-                    newDownvote = new Downvote(downVoteToCreate)
-                }
                 const downToUpdate = downvote || newDownvote;
                 downToUpdate.voters.push(req.user._id);
 
-                downToUpdate.save().then(() => {
-
-   
-                     // check if has upvoted
-                Upvote.findOne({ questionId: req.params.questionId }).populate({path: 'voters'}).exec((err, upvote) => {
+                downToUpdate.save((err) => {
                     if (err) {
-                        return res.status(500).json({ data: `error downvoting question: ${err}` }) 
-                    }
-                    if(upvote) {
-                        for (let i = 0; i < upvote.voters.length; i++) {
-                            if (req.user._id.toString() === upvote.voters[i]._id.toString()) {
-                                console.log('user already upvoted')
-                                hasupvoted = true;
-                                // delete upvote
-                                Question.updateOne({_id: req.params.questionId,},{ $inc: { voteCount: -2 }}).then(() => {
-                                         // delete voter document from upvote
-                                         Upvote.updateOne({
-                                            questionId: req.params.questionId,
-                                        },
-                                            {
-                                                $pull: {
-                                                    voters: mongoose.Types.ObjectId(req.user._id) 
-                                                }
-                                            }, (err) => {
-                                                // question.voteCount += incrBy;
-                                                return res.status(200).json({ 
-                                                    message: 'Question successfully negated upvote',
-                                                    data: votedQuestion 
-                                                });
-                                            });
-                                    }).catch(err => {
-                                        return res.status(500).json({ data: `error upvoting question: ${err}` }) 
-                                    });
-                                    break;
-                            }
+                        return res.status(500).json({ data: 'error saving new upvote' + err })
+                    } 
+                    Upvote.findOne({ questionId: req.params.questionId }).populate('voters').exec((err, upvote) => {
+                        if (err) {
+                            return res.status(500).json({ data: `error while returning upvote: ${err}` }) 
                         }
-                    } else {
-                        console.log('it fotr to push')
-                        votedQuestion.voteCount -= 1;
-                        votedQuestion.save((err) => {
-                            if (err) {
-                                return res.status(500).json({ data: `error downvoting question: ${err}` })
-                            } else {
-                                return res.status(200).json({ message: 'Question successfully downvoted',
-                                data: votedQuestion })
+                        if(upvote) {
+                            for (let i = 0; i < upvote.voters.length; i++) {
+                                if (req.user._id.toString() === upvote.voters[i]._id.toString()) {
+                                    hasupvoted = true;
+                                    // delete voter document from upvote
+                                    Upvote.updateOne({
+                                        questionId: req.params.questionId,
+                                    },
+                                        {
+                                            $pull: {
+                                                voters: mongoose.Types.ObjectId(req.user._id) 
+                                            }
+                                        }, (err) => {
+
+                                            Question.updateOne({_id: req.params.questionId},{ $inc: { voteCount: -2 }}).then(() => {
+                                                            return res.status(200).json({ 
+                                                                message: 'Question successfully negated upvote',
+                                                                data: votedQuestion 
+                                                            });
+                                            }).catch(err => {
+                                                return res.status(500).json({ data: `error upvoting question: ${err}` }) 
+                                            });
+                                        });
+                                    break;
+                                }
                             }
-                        });
-                    }  
-                });  
+                        } 
+                        if (!hasupvoted) {
+                            votedQuestion.voteCount -= 1;
+                            votedQuestion.save((err) => {
+                                if (err) {
+                                    return res.status(500).json({ data: `error downvoting question: ${err}` })
+                                } else {
+                                    return res.status(200).json({ message: 'Question successfully downvoted',
+                                    data: votedQuestion })
+                                }
+                            });
+                        }
+                    })  
+                })
             }
         })
         
     },
 
     search (req, res) {
-        // db.inventory.find( { "instock": { warehouse: "A", qty: 5 } } )
         console.log('req.query', req.query)
 
-        // const filter = { answers: { answer: req.query.search } };
-        // {
-        //     query: { $regex : `.*${req.query.search}.*` },
-        //     tag: { $regex : `.*${req.query.tag}.*` },
-        //     answers: { answer: `.*${req.query.answer}.*` } 
-        // }
-        const filter = 
-                        {   
-                                query: { $regex : `.*${req.query.search}.*` } || '',
-                                tag: { $regex : `.*${req.query.tag}.*` } || '',
-                                answers: { answer: `.*${req.query.answer}.*` } || '', 
-                     };
+        const filter = {   
+            $text: { $search: `.*${req.query.search}.*` } 
+        };
+
     
-        Question.find(filter, (err, questions) => {
+        Question.find(filter).populate('answers').exec((err, questions) => {
             if (err) {
                 return res.status(500).json({ data: 'error getting questions:' + err })
             } else {
@@ -412,9 +404,9 @@ module.exports = {
     },
     /**
      * Question middleware
-     */
+    */
     questionByID (req, res, next, id) {
-        Question.findById(req.params.questionId).populate('user').populate({path: 'answers'}).populate({path: 'upvotes'}).populate({path: 'upvotes'}).exec((err, question) => {
+        Question.findById(req.params.questionId).populate('user').populate('answers').exec((err, question) => {
             if (err) return next(err);
             if (!question) return next(new Error('Failed to load question ' + id));
             req.question = question;
